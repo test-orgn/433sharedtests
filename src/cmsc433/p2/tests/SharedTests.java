261657f26b03b7947c0697fe80b94296d7a3e00c
Lucas Ouellette-Falkenstein <lfalkenstein9@gmail.com>
2017-10-08 18:30:46 -0400

    Add shared tests file.
    Add new Test methods here unless they are special enough to require a
    seperate class.

---
+++ src/cmsc433/p2/tests/SharedTests.java
@@ -0,0 +1,27 @@
+package cmsc433.p2.tests;
+import cmsc433.p2.Simulation;
+import cmsc433.p2.Validate;
+import org.junit.Test;
+
+public class SharedTests {
+    
+@Test
+public void TestSimpleRun() {		
+	
+	
+	int numCustomers = 1;
+	int numCooks = 1;
+	int numTables = 1;
+	int machineCapacity = 1;
+	boolean randOrders = false;
+	
+	Validate.validateSimulation(
+			Simulation.runSimulation(
+					numCustomers, numCooks, 
+					numTables, machineCapacity,
+					randOrders
+					)
+			);
+	
+}
+}
